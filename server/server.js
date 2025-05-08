9f4f51034c8554df55af5b3c4668b0d56f90bf45
SimhaVarma-software <simhavarma79@gmail.com>
2025-05-08 09:38:25 +0530

    get-request

---
+++ server/server.js
@@ -0,0 +1,25 @@
+// server.js
+
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+// Initialize the app
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Sample route
+app.get('/', (req, res) => {
+  res.send('Backend server is running!');
+});
+
+// Set the port
+const PORT = process.env.PORT || 5000;
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
