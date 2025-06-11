ab310997df31046edabf012b72297be2fb11564b
SimhaVarma-software <simhavarma79@gmail.com>
2025-06-11 17:46:24 +0530

    capstone

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,13 @@ const authRoutes = require('./routes/auth');
 app.use(cors());
 app.use(express.json());
 
+
 // MongoDB Connection
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/movieapp', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
+
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
@@ -24,7 +26,6 @@ db.once('open', () => {
 });
 
 app.use("/", authRoutes);
-
 // Example route
 app.get('/', (req, res) => {
     res.send('Movie Application Backend is running');
